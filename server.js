84050cb0bc7867c104df878c116951030e6c88a3
yasin-arafat-389 <yasin.arafat.rwd@gmail.com>
2023-10-22 17:25:21 +0600

    Configured the server

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 5001;
+
+// Middlewares
+app.use(express.json());
+app.use(cors());
+
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.gef2z8f.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  const providersCollection = client.db("DineDash").collection("providers");
+
+  try {
+    // Get Brand names and images API
+    app.get("/providers", async (req, res) => {
+      const result = await providersCollection.find().toArray();
+      res.send(result);
+    });
+
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Server is up and running");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
