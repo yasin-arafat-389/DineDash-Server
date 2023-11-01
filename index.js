eacf59d8933ccb8c0a45cff5ddba874b5dce1570
yasin-arafat-389 <yasin.arafat.rwd@gmail.com>
2023-11-01 15:53:47 +0600

    Added a get route to get restaurants details on the homepage

--- index.js
+++ index.js
@@ -6,7 +6,12 @@ const port = process.env.PORT || 5001;
 
 // Middlewares
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://dine-dash-client.web.app"],
+    credentials: true,
+  })
+);
 
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.gef2z8f.mongodb.net/?retryWrites=true&w=majority`;
@@ -21,11 +26,18 @@ const client = new MongoClient(uri, {
 
 async function run() {
   const providersCollection = client.db("DineDash").collection("providers");
+  const restaurantsCollection = client.db("DineDash").collection("restaurants");
 
   try {
-    // Get Brand names and images API
+    // Get Provider names and images API
     app.get("/providers", async (req, res) => {
       const result = await providersCollection.find().toArray();
+      res.send(result);
+    });
+
+    // Get Restaurents for homepage slider API
+    app.get("/restaurants", async (req, res) => {
+      const result = await restaurantsCollection.find().toArray();
       res.send(result);
     });
 
@@ -42,5 +54,5 @@ app.get("/", (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
