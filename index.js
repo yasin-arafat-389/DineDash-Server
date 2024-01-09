deea40a30f7e3b3eedd09b014c5e711cdec95d30
yasin-arafat-389 <yasin.arafat.rwd@gmail.com>
2024-01-09 13:15:59 +0600

    Fixed a bug regarding invoice sending by email

--- index.js
+++ index.js
@@ -129,7 +129,7 @@ async function run() {
       let order = req.body;
       await ordersCollection.insertOne(order);
 
-      // sendInvoice(order, order.email, order.name);
+      sendInvoice(order, order.email, order.name);
 
       res.send({ success: true });
     });
@@ -145,8 +145,8 @@ async function run() {
         total_amount: `${order.orderTotal}`,
         currency: "BDT",
         tran_id: transactionId,
-        success_url: `http://localhost:5000/payment/success/${transactionId}/${order.randString}`,
-        fail_url: `http://localhost:5000/payment/failed`,
+        success_url: `https://dine-dash-server.vercel.app/payment/success/${transactionId}/${order.randString}`,
+        fail_url: `https://dine-dash-server.vercel.app/payment/failed`,
         cancel_url: "http://localhost:3030/cancel",
         ipn_url: "http://localhost:3030/ipn",
         shipping_method: "Courier",
@@ -186,7 +186,7 @@ async function run() {
 
         await ordersCollection.insertOne(orderToCommit);
 
-        // sendInvoice(orderToCommit, orderToCommit.email, orderToCommit.name);
+        sendInvoice(orderToCommit, orderToCommit.email, orderToCommit.name);
 
         let redirectTo;
         if (orderToCommit.cartFood?.length > 0) {
@@ -195,11 +195,13 @@ async function run() {
           redirectTo = "customMadeBurgers";
         }
 
-        res.redirect(`http://localhost:5173/order-success/${redirectTo}`);
+        res.redirect(
+          `https://dine-dash-client.web.app/order-success/${redirectTo}`
+        );
       });
 
       app.post("/payment/failed", async (req, res) => {
-        res.redirect("http://localhost:5173/payment-cancelled");
+        res.redirect("https://dine-dash-client.web.app/payment-cancelled");
       });
     });
 
